d32651b261fa3329c28a3f1f982e29eb66ed7ad9
sunjingjing <http://kf.xiotspace.com:3000/>
2021-03-22 14:51:39 +0800

    模版去除不需要的元素

--- src/router/config.js
+++ src/router/config.js
@@ -229,22 +229,6 @@ const options = {
             }
           },
           component: () => import('@/pages/form/basic')
-        },
-        {
-          name: 'Ant Design Vue',
-          path: 'antdv',
-          meta: {
-            icon: 'ant-design',
-            link: 'https://www.antdv.com/docs/vue/introduce-cn/'
-          }
-        },
-        {
-          name: '使用文档',
-          path: 'document',
-          meta: {
-            icon: 'file-word',
-            link: 'https://iczer.gitee.io/vue-antd-admin-docs/'
-          }
         }
       ]
     },

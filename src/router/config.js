61e0982c275ea5b6a8fe3a4ffc95bd842656679f
sunjingjing <http://kf.xiotspace.com:3000/>
2021-03-31 15:25:50 +0800

    router config.js router.map.js 修改

--- src/router/config.js
+++ src/router/config.js
@@ -1,6 +1,5 @@
 import TabsView from "@/layouts/tabs/TabsView";
 import BlankView from "@/layouts/BlankView";
-import PageView from "@/layouts/PageView";
 
 // 路由配置
 const options = {
@@ -68,56 +67,6 @@ const options = {
               path: "analysis",
               name: "分析页",
               component: () => import("@/pages/dashboard/analysis"),
-            },
-          ],
-        },
-        {
-          path: "exception",
-          name: "异常页",
-          meta: {
-            icon: "warning",
-          },
-          component: BlankView,
-          children: [
-            {
-              path: "404",
-              name: "Exp404",
-              component: () => import("@/pages/exception/404"),
-            },
-            {
-              path: "403",
-              name: "Exp403",
-              component: () => import("@/pages/exception/403"),
-            },
-            {
-              path: "500",
-              name: "Exp500",
-              component: () => import("@/pages/exception/500"),
-            },
-          ],
-        },
-        {
-          path: "components",
-          name: "内置组件",
-          meta: {
-            icon: "appstore-o",
-          },
-          component: PageView,
-          children: [
-            {
-              path: "taskCard",
-              name: "任务卡片",
-              component: () => import("@/pages/components/TaskCard"),
-            },
-            {
-              path: "palette",
-              name: "颜色复选框",
-              component: () => import("@/pages/components/Palette"),
-            },
-            {
-              path: "table",
-              name: "高级表格",
-              component: () => import("@/pages/components/table"),
             },
           ],
         },
